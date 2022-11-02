79c1aefba6f31add1b05bc45f6246350bf906bb3
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-11-02 10:17:02 +0000

    Six Commit: Generate getters and setters for variables

--- src/main/java/com/payrollapplication/payroll/Employee.java
+++ src/main/java/com/payrollapplication/payroll/Employee.java
@@ -16,6 +16,30 @@ public class Employee {
 		this.name = name;
 		this.role = role;
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
 	
 	
 }
