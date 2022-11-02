5e49945e48e0809eb6cac9bd4507ebe0b0addeff
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-11-02 07:15:07 +0000

    Initial Project Commit!

---
+++ src/main/java/com/payrollapplication/EmployeePayrollManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.payrollapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payrollapplication/EmployeePayrollManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payrollapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
