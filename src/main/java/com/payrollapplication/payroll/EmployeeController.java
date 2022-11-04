5c7d0b6d3889e5771a56100ef78b929fbfc1c744
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-11-04 15:54:56 +0000

    Sixteen Commit: Implement variable & constructor for EmployeeController Class

--- src/main/java/com/payrollapplication/payroll/EmployeeController.java
+++ src/main/java/com/payrollapplication/payroll/EmployeeController.java
@@ -1,5 +1,16 @@
 package com.payrollapplication.payroll;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmployeeController {
+	
+	private final EmployeeRepository repository;
+
+	public EmployeeController(EmployeeRepository repository) {
+		this.repository = repository;
+	}
+	
+
 
 }
