7e90875868f024388e09d2cbc919df86b9e8758a
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-11-07 06:43:25 +0000

    Twenty-Two Commit: Implement replaceEmployee() in EmployeeController Class

--- src/main/java/com/payrollapplication/payroll/EmployeeController.java
+++ src/main/java/com/payrollapplication/payroll/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,4 +34,33 @@ public class EmployeeController {
 		return repository.findById(id)
 				.orElseThrow(() -> new EmployeeNotFoundException(id) );
 	}
+	
+	@PutMapping("/employees/{id}")
+	Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+		return repository.findById(id)
+				.map(employee -> {
+					employee.setName(newEmployee.getName());
+					employee.setRole(newEmployee.getRole());
+					return repository.save(employee);
+				})
+				.orElseGet(() -> {
+					newEmployee.setId(id);
+					return repository.save(newEmployee);
+				});	
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
