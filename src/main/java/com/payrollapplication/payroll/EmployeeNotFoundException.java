abae2143cebf3ec7476ea502210e1c2200df28cf
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-11-07 07:24:52 +0000

    Twenty-Four Commit: Implement EmployeeNotFoundException Class

--- src/main/java/com/payrollapplication/payroll/EmployeeNotFoundException.java
+++ src/main/java/com/payrollapplication/payroll/EmployeeNotFoundException.java
@@ -3,7 +3,7 @@ package com.payrollapplication.payroll;
 public class EmployeeNotFoundException extends RuntimeException{
 	
 	public EmployeeNotFoundException(Long id) {
-		super("Could not find employee " + id);
+		super("Could not find employee: " + id);
 	}
 
 }
