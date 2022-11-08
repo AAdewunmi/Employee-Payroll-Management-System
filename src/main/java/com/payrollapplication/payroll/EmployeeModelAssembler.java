f66a4d1a494b609dd397e9f9d033bd95e12a83ff
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-11-08 08:51:21 +0000

    Twenty-Nine Commit: Implement EmployeeModelAssembler Class

--- src/main/java/com/payrollapplication/payroll/EmployeeModelAssembler.java
+++ src/main/java/com/payrollapplication/payroll/EmployeeModelAssembler.java
@@ -1,5 +1,21 @@
 package com.payrollapplication.payroll;
 
-public class EmployeeModelAssembler {
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+@Component
+public class EmployeeModelAssembler implements RepresentationModelAssembler<Employee, EntityModel<Employee>>{
+
+	@Override
+	public EntityModel<Employee> toModel(Employee employee) {
+		
+		return EntityModel.of(employee, 
+				linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+				linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+		
+	}
 
 }
