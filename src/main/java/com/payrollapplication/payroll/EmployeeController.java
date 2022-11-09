a0f172b0c5d7a2b03dfc1625a7bf4916f6960def
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-11-09 08:24:05 +0000

    Thirty-Six Commit: Refactor replaceEmployee() to handle "old" and "new" client requests

--- src/main/java/com/payrollapplication/payroll/EmployeeController.java
+++ src/main/java/com/payrollapplication/payroll/EmployeeController.java
@@ -55,8 +55,8 @@ public class EmployeeController {
 	}
 	
 	@PutMapping("/employees/{id}")
-	Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
-		return repository.findById(id)
+	ResponseEntity<?> replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+		Employee updatedEmployee = repository.findById(id)
 				.map(employee -> {
 					employee.setName(newEmployee.getName());
 					employee.setRole(newEmployee.getRole());
@@ -66,6 +66,11 @@ public class EmployeeController {
 					newEmployee.setId(id);
 					return repository.save(newEmployee);
 				});	
+		
+		EntityModel<Employee> entityModel = assembler.toModel(updatedEmployee);
+		return ResponseEntity
+				.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+				.body(entityModel);
 	}
 	
 	@DeleteMapping("/employees/{id}")
